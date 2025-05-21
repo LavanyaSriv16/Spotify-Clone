16e846b93664e14e147f6793ae7ca0b526b568e8
Lavanya Srivastava <lavasrivastava765@gmail.com>
2025-05-21 12:51:23 +0530

    Add new files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Spotify-Clone
+
 Developed a Spotify Home page Clone showing all Options, songs recommendations.
+
+# Hello DEMOO timee
+
+what's Up buddy
---
+++ demo.js
@@ -0,0 +1,2 @@
+// Hello everyone! This is a demo of the Spotify Clone project.
+// In this project, we have created a music streaming application that allows users to listen to their favorite songs, create playlists, and discover new music.
