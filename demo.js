5c1036bfb92d91251f778ef8038e69e93dd3bac5
Lavanya Srivastava <lavasrivastava765@gmail.com>
2025-05-21 16:27:11 +0530

    Delete demo.js

--- demo.js
+++
@@ -1,2 +0,0 @@
-// Hello everyone! This is a demo of the Spotify Clone project.
-// In this project, we have created a music streaming application that allows users to listen to their favorite songs, create playlists, and discover new music.
